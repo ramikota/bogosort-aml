a64897cc751a9ba2d4ae893861a13db81ab6cdb8
habibullah777 <93930438+habibullah777@users.noreply.github.com>
2024-11-07 14:36:24 +0000

    More sophisticated architecture insluding all he necessary models for MongoDB

--- Backend/server.js
+++ Backend/server.js
@@ -1,19 +1,8 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000
-const connectDB = require('/Users/habib/Documents/bogosort-aml/Database/dbConn')
-const mongoose = require ('mongoose')
-mongoose.set('strictQuery', true)
 
-//Connect TO Mongo DB
-connectDB()
-mongoose.connection.once('open', ()=> {
-    console.log("Connected to MongoDB")
-});
 
-app.get("/", (req, res)=>{
-    console.log("Response sent")
-    res.send("Hello World")})
 
 
 
