c13cfe309ad4bd8f0211157561238d026bcf742a
Rami Kotaiche <158177554+ramikota@users.noreply.github.com>
2024-11-18 14:48:50 +0000

    #11

--- frontend/src/pages/OpeningPage.js
+++ frontend/src/pages/OpeningPage.js
@@ -3,6 +3,13 @@ import { useNavigate } from 'react-router-dom';
 import "../styles/OpeningPage.css"; 
 
 function OpeningPage() {
+    const mediaItems = [
+        { id: 1, title: "Harry Potter And The Deathly Hallows", imageUrl: "/media/book1.jpg" },
+        { id: 2, title: "James And The Giant Peach", imageUrl: "/media/book2.jpg" },
+        { id: 3, title: "The Imperfections Of Memory", imageUrl: "/media/book3.jpg" },
+        { id: 4, title: "A Million To One", imageUrl: "/media/book4.jpg" },
+        { id: 5, title: "The Lord Of The Rings", imageUrl: "/media/book5.jpg" }
+    ];
     const navigate = useNavigate();
 
     const handleNavigation = (path) => {
@@ -50,6 +57,14 @@ function OpeningPage() {
             <div className="content">
                     <h2>Home</h2>
                     <p>Library</p>
+                    <div className="media-grid">
+                    {mediaItems.map((item) => (
+                        <div key={item.id} className="media-item">
+                            <img src={item.imageUrl} alt={item.title} className="media-image" />
+                            <p className="media-title">{item.title}</p>
+                        </div>
+                    ))}
+                </div>
             </div>
             </div>
         </div>
