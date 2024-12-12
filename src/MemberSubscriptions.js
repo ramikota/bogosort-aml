0e3b5fbc5c5f53490c84db4e4d24d1cefa054e4d
unknown <62992468+casey-tk-cheung@users.noreply.github.com>
2024-12-12 13:43:49 +0000

    updated stylesheet

--- src/MemberSubscriptions.js
+++ src/MemberSubscriptions.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import PaymentHistory from './PaymentHistory';
-import "./stylesheet.css";
+import "./AccountantPage.css";
 import * as XLSX from 'xlsx';
 
 function MemberSubscriptions() {
